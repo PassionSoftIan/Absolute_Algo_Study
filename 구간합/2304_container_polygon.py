12145b0303779c672089547087632de73146ebbf
권인식 <passionsoft.ian@gmail.com>
2024-01-16 17:59:51 +0900

    구간합|240116

--- 구간합/2304_container_polygon.py
+++ 구간합/2304_container_polygon.py
@@ -1,5 +1,24 @@
 import sys
 sys.stdin = open("2304_container_polygon_input.txt")
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
 N = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+arr.sort()
+
+result = 0
+
+max_high = arr[0][1]
+x = arr[0][0]
+
+for i in range(N - 1):
+    if arr[i][1] < arr[i+1][1]:
+        result += (arr[i+1][0] - x) * max_high
+        x = arr[i+1][0]
+        max_high = arr[i+1][1]
+
+
+
+print(arr)
