3da689b5c6ff511e40e46771d47719a294d2f5dd
권인식 <passionsoft.ian@gmail.com>
2024-02-14 18:00:14 +0900

    DP | 240214

--- DP/1149_RGB_street.py
+++ DP/1149_RGB_street.py
@@ -2,23 +2,28 @@ import sys
 sys.stdin = open("1149_RGB_street_input.txt")
 
 
-def backtracking(depth, prev, total):
-    global result
+def backtracking(depth, prev):
     if depth == N:
-        result = min(result, total)
-        return
+        return 0
+
+    if dp[depth][prev] != -1:
+        return dp[depth][prev]
+
+    min_cost = int(1e9)
 
     for i in range(3):
         if i == prev:
             continue
-        backtracking(depth + 1, i, total + arr[depth][i])
+        cost = arr[depth][i] + backtracking(depth + 1, i)
+        min_cost = min(min_cost, cost)
+
+    dp[depth][prev] = min_cost
+    return dp[depth][prev]
 
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-dp = [int(1e9)] * N
-result = 1e9
+dp = [[-1] * 3 for _ in range(N)]
 
-backtracking(0, -1, 0)
 
-print(result)
+print(backtracking(0, -1))
